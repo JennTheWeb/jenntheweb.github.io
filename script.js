90ae4a24c1806480dd43884b7abde6333818802f
Jenn Carr <jenncarr530@gmail.com>
2017-02-19 22:11:47 -0500

    Add basic lightwindow gallery functionality

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
-$(function() {
-    $(h1).addClass("test");
+$(document).ready(function() {
+	
+	var content;
+	var openTrigger = 'item';
+	var closeTrigger = 'close'; 
+	
+	createOverlay();
+	
+	$('.' + openTrigger).on('click', function() {
+		content = 'content-' + this.id;
+		openContent(content);
+	});
+	$('.' + closeTrigger).on('click', function() {
+		closeContent(content);
+	});
+	
+	function createOverlay() {
+		$('body').append(
+			'<div class="overlay ' + closeTrigger + 
+			'" style="display: none;"></div>'
+		);
+	}
+
+	function openContent(content) {
+		$('#' + content).show();
+		showOverlay();
+	}
+	function closeContent(content) {
+		$('#' + content).hide();
+		hideOverlay();
+	}
+	
+	function showOverlay() {
+		$('.overlay').show();
+	};
+	function hideOverlay() {
+		$('.overlay').hide();
+	};
+	
+	
 });
