3065afd927da3678a4fd97d1713e0c2bbb53d3b3
Jenn Carr <jenncarr530@gmail.com>
2017-02-20 23:28:52 -0500

    Add previous and next buttons to lightwindow gallery

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 $(document).ready(function() {
 	
 	var content;
-	var openTrigger = 'item';
-	var closeTrigger = 'close'; 
+	var overlay;
+	var openTrigger = '.item';
+	var closeTrigger = '.close'; 
+	var nextTrigger = '.next';
+	var prevTrigger = '.prev';
 	
 	createOverlay();
 	
-	$('.' + openTrigger).on('click', function() {
-		content = 'content-' + this.id;
-		openContent(content);
+	$('.item').on('click', function() {
+		//TODO: make content value more automatic and scalable
+		content = '#content-' + this.id;
+		openContent();
 	});
-	$('.' + closeTrigger).on('click', function() {
-		closeContent(content);
+	$(closeTrigger).on('click', function() {
+		closeContent();
+	});
+	$(nextTrigger + ',' + prevTrigger).on('click', function() {
+		var action = $(this).attr('class');
+		showPrevOrNext(action);
 	});
 	
 	function createOverlay() {
@@ -19,23 +27,31 @@ $(document).ready(function() {
 			'<div class="overlay ' + closeTrigger + 
 			'" style="display: none;"></div>'
 		);
+		overlay = '.overlay';
 	}
 
-	function openContent(content) {
-		$('#' + content).show();
-		showOverlay();
+	function openContent() {
+		$(content).show();
+		$(overlay).show();
 	}
-	function closeContent(content) {
-		$('#' + content).hide();
-		hideOverlay();
+	function closeContent() {
+		$(content).hide();
+		$(overlay).hide();
 	}
 	
-	function showOverlay() {
-		$('.overlay').show();
-	};
-	function hideOverlay() {
-		$('.overlay').hide();
-	};
-	
-	
+	function showPrevOrNext(action) {
+		$(content).hide();
+		var change;
+		if (action == 'prev') {
+			change = -1;
+		} else {
+			change = 1;
+		}
+		//TODO: make content.split scalable, not specific to #content-item
+		var n = parseInt(
+			content.split('#content-item').join('')
+		) + change;
+		content = '#content-item' + n;
+		$(content).show();
+	}
 });
