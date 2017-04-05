b3a9da89d0713fbb7947feeee7d017b8eadae1d3
Jenn Carr <jenncarr530@gmail.com>
2017-04-04 22:46:46 -0400

    major refactor to increase modularity and reduce number of loops and size of code. Gotta get control of that spaghetti code.

--- script.js
+++ script.js
@@ -1,237 +1,164 @@
-// LIGHTBOX GALLERY
 
-$(document).ready(function() {
+//------------------//
+// LIGHTBOX GALLERY //
+//------------------//
 	
-	// FIND ALL GALLERY ITEMS AND LINK THEM TOGETHER
+	var gallery = '.js-gallery';
+	var trigger = '.js-trigger';
+	var content = '.js-content';
+	var triggerPrefix = 'trigger-';
+	var contentPrefix = 'content-';
+	var numItems = portfolio.length;
 
-	var item = '.galleryItem';
-	var content = '.js-galleryContent';
-	var itemIdPrefix = 'item-';
-	var contentSuffix = '-content';
-	var numItems = 0;
+// CREATE HTML FOR GALLERY ITEMS
 
-	$('.js-gallery').each(function() {
-		linkPrevAndNext();
+	$(gallery).each(function() {
+		generateContent(this);
 		linkTriggerToContent();
+		linkPrevAndNext();
+		createOverlay();
 	});
 
-	function linkPrevAndNext() {	
-		numItems = 0;
-		$(item).each(function() {	
-			numItems++;
-			$(this).attr('id', itemIdPrefix + numItems);
-			$(this).attr('prevItem', '#' + itemIdPrefix + (numItems - 1));
-			$(this).attr('nextItem', '#' + itemIdPrefix + (numItems + 1));
+	function generateContent(gallery) {
+		let thumb = '';
+		let content = '';
+		let login = '';
+
+		for (var i = 0; i < numItems; ++i) {
+			if('thumbProtected' in portfolio[i]) {
+				login = '<a class="js-trigger js-trigger-authenticate trigger-authenticate" content="authentication">login to view</a>';
+			}
+
+			thumb = portfolio[i].thumbPublic;
+			content = portfolio[i].contentPublic;
+
+			$(gallery).append(
+				'<div class="galleryItem js-sliderItem">' +
+					'<a class="thumb js-trigger">' +  
+						'<img src="images/portfolio/' +
+							thumb +
+						'" />' +
+					'</a>' +
+					login +
+				'</div>' + 
+				'<div class="galleryContent js-content js-galleryContent">' + 
+					'<div class="galleryContentWrap">' + 
+						content + 
+					'</div>' +
+				'</div>'
+			);
+		}
+	}
+
+	// LINK TRIGGERS AND CONTENT
+	function linkTriggerToContent() {	
+		let n = 0;
+		$('.thumb' + trigger).each(function() {	
+			$(this).attr('id', triggerPrefix + n); // needed to reveal protected content
+			$(this).attr('content', contentPrefix + n);
+			n++;
 		});
 	}
-	function linkTriggerToContent() {
-		numItems = 0;
-		$(content).each(function() {
-			numItems++;
-			$(this).attr('id', itemIdPrefix + numItems + contentSuffix);	
-		});	
-	}
-	
-	// ADD NAV TO LIGHTWINDOW GALLERY ITEMS
+	function linkPrevAndNext() {
+		let n = 0;
+		$('.js-galleryContent' + content).each(function() {
+			$(this).attr('id', contentPrefix + n);
 
-	addNav();
-	function addNav() {		
-		for (n = 1; n <= numItems; n++) {
-			var itemId = '#' + itemIdPrefix + n;
-			var content = itemId + contentSuffix;
-			if (n != numItems) {
-				$(content).prepend(
+			if (n < numItems - 1) {
+				$(this).attr('nextItem', contentPrefix + (n + 1) );
+				$(this).prepend(
 					'<button id="next" class="galleryNext">' + 
 					'	<svg><use xlink:href="#icon-arrow-R"/></svg>' + 
 					'</button>'
 				);	
 			}
-			if (n != 1) {		
-				$(content).prepend(
+			if (n > 0) {		
+				$(this).attr('prevItem', contentPrefix + (n - 1) );
+				$(this).prepend(
 					'<button id="prev" class="galleryPrev">' + 
 					'	<svg><use xlink:href="#icon-arrow-L"/></svg>' + 
 					'</button>'
 				);
 			}
-			$(content).prepend(
+			$(this).prepend(
 				'<button id="close" class="galleryClose" aria-label="Close">' + 
 				'	<svg><use xlink:href="#icon-close"/></svg>' +
 				'</button>'
 			);
-		};
-	}
-	
-	// OPEN AND CLOSE LIGHTWINDOW CONTENT
 
-	var currentItem;
-	
-	$(item + ' .trigger').on('click', function() {
-		currentItem = '#' + $(this).parent(item).attr('id');
-		currentItemContent = currentItem + contentSuffix;
-		$(currentItemContent).show();
-		$(overlay).show();
-		$('body').css(
-			'overflow', 'hidden'
-		);
-	});
-	
-	$(content + ' #close').on('click', function() {
-		//TODO: create function to use for close button and overlay
-		$(currentItemContent).hide();
-		$(overlay).hide();
-		$('body').css(
-			'overflow', 'auto'
-		);
-	});
-	
-	// SHOW NEXT AND PREVIOUS LIGHTWINDOW CONTENT
-	
-	$('#next, #prev').on('click', function() {
-		var action = $(this).attr('id');
-		showPrevOrNext(action);
-	});
-
-	function showPrevOrNext(action) {
-		$(currentItemContent).hide();
-		
-		currentItem = $(currentItem).attr(action + 'Item');
-		currentItemContent = currentItem + contentSuffix;
-		
-		$(currentItemContent).show();
+			n++;
+		});	
 	}
-	
-	// CREATE BACKGROUND OVERLAY
-	
-	var overlay = '#overlay';
-	createOverlay();
-	
-	$(overlay).on('click', function() {
-		$(currentItemContent).hide();
-		$(overlay).hide();
-		$('body').css(
-			'overflow', 'auto'
-		);
-	});
-	
 	function createOverlay() {
 		$('body').append(
 			'<div class="galleryOverlay" id="overlay" style="display: none;"></div>'
 		);
 	}
-	
-});
 
-// LOAD GALLERY CONTENT FROM OBJECT
+// VIEW PREV/NEXT CONTENT
 
-function loadThumbContent() {
-	let thumb = '';
-	let login = '';
-	for (var i = 0; i < portfolio.length; ++i) {
-		if('thumbProtected' in portfolio[i]) {
-			login = '<a class="js-authentication-trigger">login to view</a>';
-		}
+	$('#next, #prev').on('click', function() {
+		var action = $(this).attr('id');
+		var currentContent = '#' + $(this).parents(content).attr('id');
+		$(currentContent).hide();
+		currentContent = '#' + $(currentContent).attr(action + 'Item');
+		$(currentContent).show();
+	});
 
-		thumb = portfolio[i].thumbPublic;
-		$('#gallery').append(
-			'<div class="galleryItem js-sliderItem">' +
-				'<a class="trigger"' + 'id="thumb-' + i + '">' +  
-					'<img src="images/portfolio/' +
-						thumb +
-					'" />' +
-				'</a>' +
-				login +
-			'</div>'
-		);
-	}
-}
-
-function loadContent() {
-	let content = '';
-	for (var i = 0; i < portfolio.length; ++i) {
-		content = portfolio[i].contentPublic;
-		$('#gallery').append(
-			'<div class="galleryContent js-galleryContent">' + 
-				'<div class="galleryContentWrap" id="content-' + i + '">' + 
-					content + 
-				'</div>' +
-			'</div>'
-		);
-	}
-}
-
-// PASSWORD PROTECTION
-
-function testResults(form) {
-	var password = 'pass';
-	var testVar = form.inputbox.value;
-
-	var content = '#authentication';
-	var trigger = '.js-authentication-trigger';
-	var overlay = '#overlay2';
-
-	if(testVar == password) {
-		showProtectedThumbContent();
-		showProtectedContent();
-		$(content).hide();
-		$(overlay).hide();
-		$('body').css(
-			'overflow', 'auto'
-		);
-	}
-}
-
-function showProtectedThumbContent() {
-	for (var i = 0; i < portfolio.length; ++i) {
-		let thumb = '';
-		if ('thumbProtected' in portfolio[i]) {
-			thumb = 
-				'<img src="images/portfolio/' + 
-					portfolio[i].thumbProtected + 
-				'" />';
-			$('#thumb-' + i).html(thumb);
-		}	
-	}
-}
-
-function showProtectedContent() {
-	for (var i = 0; i < portfolio.length; ++i) {
-		let content = '';
-		if ('contentProtected' in portfolio[i]) {
-			content = portfolio[i].contentProtected;
-			$('#content-' + i).html(content);
-		}	
-	}
-} 
-
-// LOGIN FORM LIGHTWINDOW
-
-$(document).ready(function() {
-	var content = '#authentication';
-	var trigger = '.js-authentication-trigger';
-	var overlay = '#overlay2';
-	createOverlay();
-
+// OPEN AND CLOSE CONTENT
 	$(trigger).on('click', function() {
+		var currentItemContent = '#' + $(this).attr('content');
+		openContent(currentItemContent);
+	});
+	$(content + ' #close, #overlay').on('click', function() {
+		closeContent(content);
+	});
+	function openContent(content) {
 		$(content).show();
-		$(overlay).show();
+		$('#overlay').show();
 		$('body').css(
 			'overflow', 'hidden'
 		);
-	});
-	$(overlay).on('click', function() {
+	}
+	function closeContent(content) {
 		$(content).hide();
-		$(overlay).hide();
+		$('#overlay').hide();
 		$('body').css(
 			'overflow', 'auto'
 		);
-	});	
-	
-	function createOverlay() {
-		$('body').append(
-			'<div class="galleryOverlay" id="overlay2" style="display: none;"></div>'
-		);
 	}
-});
+
+// PASSWORD PROTECTION
+
+	function authenticate(form) {
+		var password = 'pass';
+		var testVar = form.inputbox.value;
+
+		var form = '#authentication';
+
+		if(testVar == password) {
+			showProtectedContent();
+			closeContent(form);
+			$('.js-trigger-authenticate').hide();
+		}
+	}
+	function showProtectedContent() {
+		for (var i = 0; i < numItems; ++i) {
+			let thumb = '';
+			let content = '';
+			if ('thumbProtected' in portfolio[i]) {
+				thumb = 
+					'<img src="images/portfolio/' + 
+						portfolio[i].thumbProtected + 
+					'" />';
+				$('#' + triggerPrefix + i).html(thumb);
+			}	
+			if ('contentProtected' in portfolio[i]) {
+				content = portfolio[i].contentProtected;
+				$('#' + contentPrefix + i + ' .galleryContentWrap').html(content);
+			}	
+		}
+	} 
 
 // SLIDER
 
