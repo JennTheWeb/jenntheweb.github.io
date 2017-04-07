0bd9ff8332796723fe482e98adab09828320fd67
Jenn Carr <jenncarr530@gmail.com>
2017-04-06 22:29:05 -0400

    Add new portfolio items, Delete portfolio folder

---
+++ portfolio.js
@@ -0,0 +1,59 @@
+let portfolio = [];
+portfolio.push ({
+    thumbPublic: 'loomAndMill.jpg',
+    contentPublic:
+        '<h2>Web Design, Magento Development</h2>' +
+        '<img src="images/portfolio/loomAndMill.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'lot76NYC.jpg',
+    contentPublic:
+        '<h2>Web Design, Custom Wordpress Theme Development</h2>' +
+        '<img src="images/portfolio/lot76NYC.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'LHI.jpg',
+    contentPublic:
+        '<h2>Web Design, Drupal Integration</h2>' +
+        '<img src="images/portfolio/LHI.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'practicalBA.jpg',
+    contentPublic:
+        '<h2>Web Design, Custom Wordpress Theme Development</h2>' +
+        '<img src="images/portfolio/practicalBA.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'cs/p1-blurred.jpg',
+    thumbProtected: 'cs/p1.jpg',
+    contentPublic:
+        '<h2>Web Design, Sitefinity Integration</h2>' +
+        '<img src="images/portfolio/cs/p1-blurred.jpg" />',
+    contentProtected:
+        '<h2>Web Design, Sitefinity Integration</h2>' +
+        '<img src="images/portfolio/cs/p1.jpg" />',
+});
+portfolio.push ({
+    thumbPublic: 'snapKnot.jpg',
+    contentPublic:
+        '<h2>Web Design, HTML/CSS, PHP integration</h2>' +
+        '<img src="images/portfolio/snapKnot.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'rittenhouseNeedlepoint.jpg',
+    contentPublic:
+        '<h2>Web Design, HTML/CSS, Magento Integration</h2>' +
+        '<img src="images/portfolio/rittenhouseNeedlepoint.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'creativeHeads.jpg',
+    contentPublic:
+        '<h2>Web UI Design, HTML/CSS, ASP.NET Integration</h2>' +
+        '<img src="images/portfolio/creativeHeads.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'remository.jpg',
+    contentPublic:
+        '<h2>Web UI Design</h2>' +
+        '<img src="images/portfolio/remository.jpg" />'
+});
