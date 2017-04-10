8f3aea2e9a7ec492ddd6a5df0fb12fd459dea3c1
Jenn Carr <jenncarr530@gmail.com>
2017-04-09 21:13:44 -0400

    Add portfolio items

--- portfolio.js
+++ portfolio.js
@@ -2,7 +2,7 @@ let portfolio = [];
 portfolio.push ({
     thumbPublic: 'loomAndMill.jpg',
     contentPublic:
-        '<h2>Web Design, Magento Development</h2>' +
+        '<h2>Responsive Web Design, Magento Development</h2>' +
         '<img src="images/portfolio/loomAndMill.jpg" />'
 });
 portfolio.push ({
@@ -27,11 +27,11 @@ portfolio.push ({
     thumbPublic: 'cs/p1-blurred.jpg',
     thumbProtected: 'cs/p1.jpg',
     contentPublic:
-        '<h2>Web Design, Sitefinity Integration</h2>' +
+        '<h2>Responsive Web Design, Sitefinity Integration</h2>' +
         '<img src="images/portfolio/cs/p1-blurred.jpg" />',
     contentProtected:
-        '<h2>Web Design, Sitefinity Integration</h2>' +
-        '<img src="images/portfolio/cs/p1.jpg" />',
+        '<h2>Web Design, Sitefinity Integration, , Responsive Design</h2>' +
+        '<img src="images/portfolio/cs/p1.jpg" />'
 });
 portfolio.push ({
     thumbPublic: 'snapKnot.jpg',
@@ -56,4 +56,116 @@ portfolio.push ({
     contentPublic:
         '<h2>Web UI Design</h2>' +
         '<img src="images/portfolio/remository.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'elegantWay.jpg',
+    contentPublic:
+        '<h2>Web Design, Custom Wordpress Theme Development</h2>' +
+        '<img src="images/portfolio/elegantWay.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'MTK.jpg',
+    contentPublic:
+        '<h2>Web Design, HTML/CSS Develpment</h2>' +
+        '<img src="images/portfolio/MTK.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'cs/p2-blurred.jpg',
+    thumbProtected: 'cs/p2.jpg',
+    contentPublic:
+        '<h2>Web Design, Wordpress Theme Development, Project Management</h2>' +
+        '<img src="images/portfolio/cs/p2-blurred.jpg" />',
+    contentProtected:
+        '<h2>Web Design, Wordpress Theme Development, Project Management</h2>' +
+        '<img src="images/portfolio/cs/p2.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'cs/p3-blurred.jpg',
+    thumbProtected: 'cs/p3.jpg',
+    contentPublic:
+        '<h2>Web UI/UX Design</h2>' +
+        '<img src="images/portfolio/cs/p3-blurred.jpg" />',
+    contentProtected:
+        '<h2>Web UI/UX Design</h2>' +
+        '<img src="images/portfolio/cs/p3.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'cs/p4-blurred.jpg',
+    thumbProtected: 'cs/p4.jpg',
+    contentPublic:
+        '<h2>Web UI Design, Sitefinity Integration, Project Management, e-commerce, Responsive Design</h2>' +
+        '<img src="images/portfolio/cs/p4-blurred.jpg" />',
+    contentProtected:
+        '<h2>Web UI Design, Sitefinity Integration, Project Management, e-commerce, Responsive Design</h2>' +
+        '<img src="images/portfolio/cs/p4.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'cs/p5-blurred.jpg',
+    thumbProtected: 'cs/p5.jpg',
+    contentPublic:
+        '<h2>Responsive Web Design, Wordpress Theme Development</h2>' +
+        '<img src="images/portfolio/cs/p5-blurred.jpg" />',
+    contentProtected:
+        '<h2>Responsive Web Design, Wordpress Theme Development</h2>' +
+        '<img src="images/portfolio/cs/p5.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'cs/p6-blurred.jpg',
+    thumbProtected: 'cs/p6.jpg',
+    contentPublic:
+        '<h2>Responsive Web Design, Wordpress Theme Development</h2>' +
+        '<img src="images/portfolio/cs/p6-blurred.jpg" />',
+    contentProtected:
+        '<h2>Responsive Web Design, Wordpress Theme Development</h2>' +
+        '<img src="images/portfolio/cs/p6.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'cs/p7-blurred.jpg',
+    thumbProtected: 'cs/p7.jpg',
+    contentPublic:
+        '<h2>Responsive Web Design, Wordpress Theme Development</h2>' +
+        '<img src="images/portfolio/cs/p7-blurred.jpg" />',
+    contentProtected:
+        '<h2>Responsive Web Design, Wordpress Theme Development</h2>' +
+        '<img src="images/portfolio/cs/p7.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'cs/p8-blurred.jpg',
+    thumbProtected: 'cs/p8.jpg',
+    contentPublic:
+        '<h2>Responsive Web Design, Sitefinity Integration, e-commerce</h2>' +
+        '<img src="images/portfolio/cs/p8-blurred.jpg" />',
+    contentProtected:
+        '<h2>Responsive Web Design, Sitefinity Integration, e-commerce</h2>' +
+        '<img src="images/portfolio/cs/p8.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'ss/p1.jpg',
+    contentPublic:
+        '<h2>Web Design, Adobe Business Catalyst Integration</h2>' +
+        '<img src="images/portfolio/ss/p1.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'ss/p2.jpg',
+    contentPublic:
+        '<h2>Web Design, HTML/CSS Development</h2>' +
+        '<img src="images/portfolio/ss/p2.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'ss/p3.jpg',
+    contentPublic:
+        '<h2>Web Design, HTML/CSS Development</h2>' +
+        '<img src="images/portfolio/ss/p3.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'ss/p4.jpg',
+    contentPublic:
+        '<h2>Web Design, Adobe Business Catalyst Integration</h2>' +
+        '<img src="images/portfolio/ss/p4.jpg" />'
+});
+portfolio.push ({
+    thumbPublic: 'ss/p5-c.jpg',
+    contentPublic:
+        '<h2>Web Design, HTML/CSS</h2>' +
+        '<img src="images/portfolio/ss/p5-c.jpg" />'
 });
